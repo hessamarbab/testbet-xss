42ad6eff564781d002f9c9be32595d5ab0d5b419
Hessam Arbab <hessam.arbab@gmail.com>
2025-06-10 14:44:16 +0330

    first commit

---
+++ reflector.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+    <html>
+
+
+        <form style="margin: 20px" action="#">
+            attr : <input id="attr" type="text">
+                     <hr/>
+            encode : <input id="encode" type="text">
+                        <hr/>
+            trim : <input id="trim" type="text">
+                        <hr/>
+            ignoreHTML : <input id="ignoreHTML" type="text">
+                        <hr/>
+            ignoreQoutScript : <input id="ignoreQoutScript" type="text">
+                        <hr/>
+            ignoreScript : <input id="ignoreScript" type="text">
+                        <hr/>
+            tag : <input id="tag" type="text">
+                        <hr/>
+        </form>
+    </html>
+<?php
+
+
+
