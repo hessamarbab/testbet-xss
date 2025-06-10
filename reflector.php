969b44549522f46b65c2b86e9c163a85844d6087
Hessam Arbab <hessam.arbab@gmail.com>
2025-06-10 16:25:58 +0330

    added reflect

--- reflector.php
+++ reflector.php
@@ -3,7 +3,7 @@
 
 
         <form style="margin: 20px" action="reflector.php"  method="POST">
-            attr : <input id="attr" type="text">
+            attr : <input name="attr" type="text">
                      <hr/>
             encode : <input name="encode" type="text">
                         <hr/>
@@ -18,9 +18,29 @@
             tag : <input name="tag" type="text">
                         <hr/>
             <input type="submit" value="sub">
+            <hr/>
         </form>
-    </html>
 <?php
+    echo $_POST['attr'] ? "<input value=\"{$_POST['attr']}\">" : "";
+    $encoded = urlencode($_POST['encode']);
+    echo $_POST['encode'] ? "<input  value=\"$encoded\">" : "";
+    $trimed = trim($_POST['trim'], "\"");
+    $decoded = urldecode($trimed);
+    echo $_POST['trim'] ? "<input  value=\"$decoded\">" : "";
 
+    $ignored = str_replace('"', '\\"', $_POST['ignoreHTML']);
+    echo $_POST['ignoreHTML'] ? "<input  value=\"$ignored\">" : "";
+
+    $ignoredQout = str_replace('"', '\\"', $_POST['ignoreQoutScript']);
+    echo $_POST['ignoreQoutScript'] ? "<script>var asghar = \"$ignoredQout\"</script>" : "";
+
+    $ignoreds = str_replace(['\\','"'], ['\\\\', '\\"'], $_POST['ignoreScript']);
+    echo $_POST['ignoreScript'] ? "<script>var asghar = \"$ignoreds\"</script>" : "";
+
+    echo $_POST['tag'] ? "<div>{$_POST['tag']}</div>" : "";
+
+
+?>
+</html>
 
 
